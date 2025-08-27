7196145b9d598ced94a3c8f994123e2e2e0c9324
Adrian Siekierka <kontakt@asie.pl>
2025-08-27 17:42:55 +0200

    Revert "firmware: place SPI handler in SRAM"
    This reverts commit 6152b6bbda4bd9667b90347446ec199fa0e67b0d.

--- firmware/src/spi.c
+++ firmware/src/spi.c
@@ -160,8 +160,7 @@ void DMA1_Channel2_3_IRQHandler(void) {
 
 static volatile uint8_t native_byte_queue = 0xFF;
 
-// Place SPI IRQ handler in SRAM to make it more responsive
-__attribute__((section(".data.SPI1_IRQHandler")))
+__attribute__((aligned(32)))
 void SPI1_IRQHandler(void) {
     if (LL_SPI_IsActiveFlag_RXNE(SPI1)) {
         if (spi_mode == MCU_SPI_MODE_NATIVE) {
